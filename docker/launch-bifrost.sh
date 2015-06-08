ebfcacc24025b615586a98d2116d25804d8f6047
Bruno Cornec <bruno.cornec@hp.com>
2015-06-08 15:33:33 +0200

    Improved version
    - Remove the setup-env.sh script as we're now patching upstream for
      Docker support
    - BIFROST_UID now used to parameter the account to use

--- docker/launch-bifrost.sh
+++ docker/launch-bifrost.sh
@@ -8,16 +8,39 @@ cat >> $HOME/.gitconfig << EOF
 	email = bruno.cornec@hp.com
 [gitreview]
 	username = bruno-cornec
-[https]
-	proxy = http://web-proxy.fra.hp.com:8080
-[http]
-	proxy = http://web-proxy.fra.hp.com:8080
 EOF
-git clone https://github.com/openstack/bifrost.git
-pip install --proxy web-proxy.fra.hp.com:8080 -r bifrost/requirements.txt
+if [ "$1" != "" ]; then
+	export http_proxy=$1
+	cat >> $HOME/.gitconfig << EOF
+[http]
+	proxy = $1
+EOF
+fi
+if [ "$2" != "" ]; then
+	export https_proxy=$2
+	cat >> $HOME/.gitconfig << EOF
+[https]
+	proxy = $2
+EOF
+fi
+export BIFROST_DOCKER=YES
+
+# Local repo
+git clone https://github.com/bcornec/bifrost.git
+#git clone https://github.com/openstack/bifrost.git
+# Local modifs
+git checkout docker
+
+# Proxy management
+opt=""
+if [ "$http_proxy" != "" ]; then
+	opt="--proxy $http_proxy"
+pip install $opt -r bifrost/requirements.txt
+fi
+
 cd bifrost
-sed -i 's/aSecretPassword473z/linux1/' playbooks/inventory/group_vars/all
-../setup-env.sh
+#sed -i 's/aSecretPassword473z/linux1/' playbooks/inventory/group_vars/all
+./script/env-setup.sh
 source env-vars
 source /opt/stack/ansible/hacking/env-setup
 cd playbooks
