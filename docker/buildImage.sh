feb2fd2ddefecea3e1f53a06b10d0af84ff2cc8f
Bruno Cornec <bruno.cornec@hp.com>
2015-08-27 17:45:33 +0200

    Docker part cleanup
    Change-Id: I19eee19fa93de9e8a68c2fa13cd42ae909dc97b5

--- docker/buildImage.sh
+++ docker/buildImage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Build and tag images
+# Build and tag images
+docker rmi bifrost
 docker build -t "bifrost" .
-docker tag bifrost:latest localhost:5000/bifrost
+docker tag -f bifrost:latest localhost:5000/bifrost
